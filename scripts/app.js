156af8b178e63f1311128f578db2e172b2e28839
zahraa237 <fzahraa237@gmail.com>
2025-07-09 13:32:49 +0300

    add the duck to a random position

--- scripts/app.js
+++ scripts/app.js
@@ -6,14 +6,33 @@ function init(){
     const gridwidth = 10;
     const numberOfCells = gridwidth * gridwidth; 
 
+    let duckPosition = 4
+
+    function addDuck(){
+        cells [duckPosition].classList.add('duck') //adding the class duck to the cell
+    }
+
+    function removeDuck(){
+        cells[duckPosition].classList.remove('duck') //removing the class duck from the cell
+    }
+
+    function play(){
+        setInterval(() => {
+            removeDuck(); //removing the duck from the previous position
+            duckPosition = Math.floor(Math.random() * numberOfCells); //getting a random number between 0 and 99
+            addDuck(); //adding the duck to the new position
+
+        }, 3000) //run every 3 seconds
+    }
+
     function creatGrid(){
         //for every cell we want creat a div
         //add it to the grid
         for (let i = 0; i < numberOfCells; i++) {
             //creatig the div 
             const cell = document.createElement('div')
-            cell.classList.add('duck') //adding the pic class
-            cell.textContent = i + 1 //adding the number of the cell
+            //cell.classList.add('duck') //adding the pic class
+            cell.textContent = i //adding the number of the cell
             cells.push(cell)
             //adding it to the main div
             gridElem.appendChild(cell)
@@ -21,6 +40,7 @@ function init(){
         console.log(cells)
     }
     creatGrid();
+    play(); //starts the game
 }
 
 document.addEventListener('DOMContentLoaded', init);
